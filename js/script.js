310947cd427f0dc4f684db37dd84b2662ca0385e
tereshchenko21 <tereshchenkobox@mail.ru>
2025-01-28 22:25:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+const dropdownButton = document.getElementById('dropdown-main-button')
+const dropdown = document.getElementById('dropdown')
+const burger = document.getElementById('burger')
+const menuList = document.getElementById('menu')
+const ruLangButton = document.getElementById('drop-rus')
+const enLangButton = document.getElementById('drop-eng')
+
+const translations = {
+    en: {
+        title: 'SURVIVE AT ALL COSTS',
+        subtitle: 'Experience new social battle royale game',
+        story: 'The story',
+    }, 
+    ru: {
+        title: 'ВЫЖИТЬ ЛЮБОЙ ЦЕНОЙ',
+        subtitle: 'Испытайте новую социальную игру battle royale',
+        story: 'История',
+    }
+}
+
+let currentLang = 'en' // текущий язык
+
+dropdownButton.onclick = function() {
+    dropdown.classList.toggle('active')
+}
+
+burger.onclick = function() {
+    burger.classList.toggle('active-burger')
+    menuList.classList.toggle('open')
+}
+
+dropdown.onclick = function(event) {
+    currentLang = event.target.id === 'drop-eng' ? 'en' : 'ru' // выбор текущего языка
+    dropdown.classList.remove('active') // закрывает dropdown после нажатия на кнопку
+    translate(currentLang) 
+}
+
+function translate(currentLanguage) {
+    document.querySelectorAll('[data-translate]').forEach((currentElement) => { //ищем все элементы с дата-атрибутами и сразу перебираем их
+        const key = currentElement.getAttribute('data-translate') // потом достаем их атрибут 
+        currentElement.textContent = translations[currentLanguage][key] // и меняем значение на нужное из коллекции объектов
+    })
+}
